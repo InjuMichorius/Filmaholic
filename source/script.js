3f9d349003c157f8d06ec5d8afaa624bd885ea40
ÏnjuMichorius <Kato1234!>
2021-02-15 12:48:07 +0100

    Added function to route

--- source/script.js
+++ source/script.js
@@ -1,42 +1,41 @@
 const movieContainer = document.getElementById('movie-template');
 
-function fetchData() {
-    fetch("https://api.themoviedb.org/3/movie/popular?api_key=6b14c4c8bcc5ae16e8216d9385a1b3a9")
-        .then(response => {
-            if (!response.ok) {
-                throw Error("URL not working")
-            }
-            return response.json();
-        })
-        .then(movies => {
-            console.log(movies);
-            const html = movies.results.map(movie => {
-                return `
-                <article class="movie">
-                    <img src="https://image.tmdb.org/t/p/w200/${movie.poster_path}" alt="Cover of ${movie.title}">
-                    <p class="movie-title">${movie.title}</p>
-                    <p>${movie.release_date}</p>
-                    <p>${movie.vote_average}</p>
-                    <a href="#${movie.id}">click me</a>
-                </article>`
-            })
-                .join('')
-
-            movieContainer.insertAdjacentHTML("afterbegin", html)
-
-        })
-        .catch(error => {
-            console.log(error);
-        });
-}
-
-fetchData()
-
 //Create movie detail page
 function router() {
-    routie(':id', function (id) {
-        console.log(id)
-    });
-};
+    routie('overview', function (id) {
+        function getPopularMovies() {
+            fetch("https://api.themoviedb.org/3/movie/popular?api_key=6b14c4c8bcc5ae16e8216d9385a1b3a9")
+                .then(response => {
+                    if (!response.ok) {
+                        throw Error("URL not working")
+                    }
+                    return response.json();
+                })
+                .then(movies => {
+                    console.log(movies);
+                    const html = movies.results.map(movie => {
+                        return `
+                        <article class="movie">
+                            <img src="https://image.tmdb.org/t/p/w200/${movie.poster_path}" alt="Cover of ${movie.title}">
+                            <p class="movie-title">${movie.title}</p>
+                            <p>${movie.release_date}</p>
+                            <p>${movie.vote_average}</p>
+                            <a href="#${movie.id}">click me</a>
+                        </article>`
+                    })
+                        .join('')
 
-router()
+                    movieContainer.insertAdjacentHTML("afterbegin", html)
+
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        }
+        getPopularMovies()
+    }),
+        routie(':id', function (id) {
+            console.log(id)
+        });
+}
+router();
