8e9351bcf967ed3c0b6163eaf804f4b3974ee495
ÏnjuMichorius <Kato1234!>
2021-03-01 13:43:58 +0100

    App now console logs both movie titles and user input

--- source/search.js
+++ source/search.js
@@ -1,7 +1,23 @@
+import { fetchData } from './api.js'
+
 const input = document.getElementById('find-movies');
 
 input.addEventListener('input', search);
 
 export async function search(event) {
-    console.log(event.target.value)
+    
+    const json = await fetchData('popular');
+    const movies = json.results
+    const html = movies.map(movie => {
+        return `${movie.title}`
+    });
+
+    console.log(html)
+
+
+    let searchInput = event.target.value;
+    console.log(searchInput);
+
+
+
 };
