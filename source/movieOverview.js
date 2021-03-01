5c9f16e7ca79e7993a72a80108250af9c4a14568
ÏnjuMichorius <Kato1234!>
2021-03-01 16:50:48 +0100

    Added sorting by title name

--- source/movieOverview.js
+++ source/movieOverview.js
@@ -1,14 +1,41 @@
 import { fetchData } from './api.js'
-const loader = document.querySelector('lds-spinner')
+import { sort } from './sort.js';
+const loader = document.getElementById('lds-spinner')
 
-export async function movieOverview() {
+const alphabetical = document.querySelector('.sort');
 
+    alphabetical.addEventListener('click', ()=> {
+        movieOverview('Alpha')
+    });
+
+export async function movieOverview(sorting) {
     const json = await fetchData('popular');
     const movies = json.results
+
+    const sortedMovies = sortMovies(sorting, movies);
+
+    function sortMovies(sorting, movies) {
+        if (sorting == 'none') {
+            return movies
+        } else if (sorting == 'Alpha') {
+            return movies.sort((a, b) => {
+                const nameA = a.title.toLowerCase()
+                const nameB = b.title.toLowerCase()
+                if(nameA < nameB) {
+                    return -1
+                } if(nameA > nameB) {
+                    return 1
+                }
+                return 0
+
+            })
+        }
+    }
+    console.log(sortedMovies)
     const movieContainer = document.getElementById('movie-template');
     movieContainer.innerHTML = '';
-    const html = movies.map(movie => {
-    return `
+    const html = sortedMovies.map(movie => {
+        return `
     <article class="movie">
     <a href="#${movie.id}">
     <img src="https://image.tmdb.org/t/p/w200/${movie.poster_path}" alt="Cover of ${movie.title}">
@@ -19,7 +46,7 @@ export async function movieOverview() {
     </article>`
     }).join('')
 
-    loader.classList.toggle('hide');
+    // loader.classList.toggle('hide');
 
     movieContainer.insertAdjacentHTML("afterbegin", html)
 }
--- source/router.js
+++ source/router.js
@@ -2,6 +2,6 @@ import { movieOverview } from './movieOverview.js';
 import { movieDetails } from './movieDetails.js';
 
 export function router() {
-    routie({'': movieOverview});
+    routie({'': movieOverview('none')});
     routie({':id': movieDetails});
 };
